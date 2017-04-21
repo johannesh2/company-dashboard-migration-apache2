d54e240e43db5b3cf10aa3dc02d88b804c6dbfb6
Johannes HÃ?Â¤yry <johannes.hayry@vaadin.com>
2017-04-21 09:00:29 +0300

    Apache 2.0 branch

---
+++ README.md
@@ -0,0 +1,10 @@
+# Company Dashboard GXT sample migrated to Vaadin
+
+This is the Apache 2.0 licensed sample branch. This is not a fully functional example,
+because the GPLv3 licensed GXT sample code is stripped away.
+
+Demonstration on how to migrate a an existing GXT application to Vaadin with minimal 
+required steps. Existing functionality continues working as is, and the new
+functionality should be developed using Vaadin Framework's server-side approach.
+This sample project gives some guidance, but every migration project should be 
+considered case-by-case.
--- src/main/java/com/vaadin/example/gxt/companydashboard/client/migration/ViewWrapperWidget.java
+++ src/main/java/com/vaadin/example/gxt/companydashboard/client/migration/ViewWrapperWidget.java
@@ -1,22 +1,20 @@
 package com.vaadin.example.gxt.companydashboard.client.migration;
 
-import com.google.gwt.core.shared.GWT;
 import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
-import com.sencha.gxt.widget.core.client.ContentPanel;
-import com.vaadin.example.gxt.companydashboard.client.CompanyDashboard;
 import com.vaadin.example.gxt.companydashboard.shared.migration.Views;
 
-public class ViewWrapperWidget extends ContentPanel {
+public class ViewWrapperWidget extends SimplePanel {
 
 	public ViewWrapperWidget() {
-		setDeferHeight(false);
 	}
 
 	public void setView(String view) {
 		switch (view) {
 		case Views.VIEW_DASHBOARD:
-			doSetView("Company Dashboard", GWT.create(CompanyDashboard.class));
+			doSetView("Company Dashboard", new Label("TODO"));
 			break;
 		default:
 			break;
@@ -25,13 +23,10 @@ public class ViewWrapperWidget extends ContentPanel {
 	}
 
 	private void doSetView(String heading, IsWidget view) {
-		if (getWidgetCount() > 0) {
-			clear();
-		}
-		setHeading(heading);
+
 		if (view instanceof Widget) {
 			((Widget) view).setSize("100%", "100%");
 		}
-		add(view);
+		setWidget(view);
 	}
 }
